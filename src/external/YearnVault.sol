88ff009f5d031aeaaf75d6255b625ec3c268b95b
ZeframLou <zeframlou@gmail.com>
2022-05-13 00:27:10 -0700

    fix: lock solidity version in pragmas

--- src/external/YearnVault.sol
+++ src/external/YearnVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.13;
 
 interface YearnVault {
     function token() external view returns (address);
--- src/test/mocks/TestERC4626.sol
+++ src/test/mocks/TestERC4626.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 
-pragma solidity ^0.8.4;
+pragma solidity 0.8.13;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {ERC4626} from "solmate/mixins/ERC4626.sol";
